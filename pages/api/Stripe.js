196945bc45a9209af6ff72dc177c4930cce9f109
Toxic-Whitelgnd <whitelegend56@gmail.com>
2022-08-14 17:27:54 +0530

    Day-4

--- pages/api/Stripe.js
+++ pages/api/Stripe.js
@@ -16,6 +16,9 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
+        shipping_address_collection:{
+          allowed_countries:['IN'],
+        },
         shipping_options:[
             {
                 shipping_rate:'shr_1LWEC7SGr6vvmBhjx73YB21E',
